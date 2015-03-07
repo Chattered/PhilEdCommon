060dd38c5cff1cb03f2c6780b3b1c03c1bf9b1fa
Phil Scott <me@philscotted.com>
2015-03-07 09:51:55 +0000

    Missed from last commit.

---
+++ Philed/Control/Monad/Record.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE FunctionalDependencies, MultiParamTypeClasses,
+             GeneralizedNewtypeDeriving, FlexibleInstances, UndecidableInstances #-}
+
+-- A weaker version of State, where updates are restricted to addition for some
+-- monoid.
+
+module Philed.Control.Monad.Record (RecordT, runRecordT, evalRecordT, execRecordT
+                                   ,MonadRecord, record, get) where
+
+import Control.Applicative
+import Control.Monad.Except
+import Control.Monad.Reader
+import qualified Control.Monad.State as S
+import Data.Monoid
+
+class MonadRecord s m | m -> s where
+  record :: s -> m ()
+  get    :: m s
+
+-- RecordT adds on the right be default.
+newtype RecordT s m a = RecordT { unRecordT :: S.StateT s m a }
+                      deriving (Applicative, Functor, Monad)
+
+runRecordT :: (Monoid s, Monad m) => RecordT s m a -> m (a,s)
+runRecordT x = S.runStateT (unRecordT x) mempty
+
+evalRecordT :: (Monoid s, Monad m) => RecordT s m a -> m a
+evalRecordT x = S.evalStateT (unRecordT x) mempty
+
+execRecordT :: (Monoid s, Monad m) => RecordT s m a -> m s
+execRecordT x = S.execStateT (unRecordT x) mempty
+
+instance (Monoid s, Monad m) => MonadRecord s (RecordT s m) where
+  record x = RecordT (S.modify $ (<> x))
+  get      = RecordT S.get
+
+instance MonadTrans (RecordT s) where
+  lift = RecordT . lift
+
+instance (Monad m, MonadError e m) => MonadError e (RecordT s m) where
+  throwError     = lift . throwError
+  catchError x h = RecordT (catchError (unRecordT x) (unRecordT . h))
+
+instance (Monad m, S.MonadState s m) => S.MonadState s (RecordT t m) where
+  get = lift S.get
+  put = lift . S.put
+
+instance (Monad m, MonadReader r m) => MonadReader r (RecordT e m) where
+  ask                 = lift ask
+  local f (RecordT s) = RecordT (local f s)
+
+instance (Monad m, MonadRecord s m) => MonadRecord s (ExceptT e m) where
+  record = lift . record
+  get    = lift get
+
+instance MonadIO m => MonadIO (RecordT s m) where
+  liftIO = lift . liftIO
