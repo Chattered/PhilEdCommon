8995009d04a106ed8e1b5c43399cc7769859d79a
Phil Scott <me@philscotted.com>
2015-06-13 13:47:28 +0100

    Initial commit.

---
+++ default.nix
@@ -0,0 +1,12 @@
+{ haskellngPackages ? (import <nixpkgs> {}).haskellngPackages,
+  pkgs ? (import <nixpkgs> {}).pkgs
+}:
+let
+  env = haskellngPackages.ghcWithPackages (p: with p; [
+    array base binary comonad containers dlist mtl parsec QuickCheck
+    semigroups semigroupoids cabal-install
+  ]);
+in pkgs.stdenv.mkDerivation {
+  name = "Philed";
+  buildInputs = [ env ];
+}
