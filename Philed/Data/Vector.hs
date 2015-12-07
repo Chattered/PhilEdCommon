ca0d551978c77f3747e6e23a9cb62ea1fa8ce9e7
Phil Scott <me@philscotted.com>
2015-12-07 10:14:04 +0000

    Vector extras.
    Reflections, unit vectors and isometry composition/matrix multiplication
    as the monoid instance.

--- Philed/Data/Vector.hs
+++ Philed/Data/Vector.hs
@@ -1,12 +1,19 @@
 module Philed.Data.Vector (Vec, UnitV, Isometry
+                          ,xVec, yVec
                           ,fromUnitVector, normalise
                           ,(+.), (-.), (*.), dot, cross, norm, magnitude
                           ,project, projectUnit, projectK, projectUnitK
-                          ,rot, rot90
+                          ,rot, rot90, reflX, refl
                           ,applyIso, applyIsoU) where
 
-newtype UnitV a = UnitV (a,a)
+newtype UnitV a = UnitV (a,a) deriving (Eq, Show)
 type Vec a = (a,a)
+
+xVec :: Num a => UnitV a
+xVec = UnitV (1,0)
+
+yVec :: Num a => UnitV a
+yVec = UnitV (0,1)
 
 fromUnitVector :: UnitV a -> (a,a)
 fromUnitVector (UnitV v) = v
@@ -50,13 +57,24 @@ projectUnit u v = projectUnitK u v *. fromUnitVector u
 normalise :: Floating a => (a,a) -> UnitV a
 normalise v = UnitV (v /. magnitude v)
 
-newtype Isometry  a = MatrixIso (a,a,a,a)
+newtype Isometry a = MatrixIso (a,a,a,a)
+
+instance Num a =>  Monoid (Isometry a) where
+  mempty = MatrixIso (1,0,0,1)
+  mappend (MatrixIso (a,b,c,d)) (MatrixIso (e,f,g,h)) =
+    MatrixIso (a*e+b*g, a*f+b*h, c*e+d*g, c*f+d*h)
 
 rot :: Floating a => a -> Isometry a
 rot θ = MatrixIso (cos θ, -(sin θ), sin θ, cos θ)
 
+refl :: Floating a => a -> Isometry a
+refl θ = MatrixIso (cos (2*θ), (sin (2*θ)), sin (2*θ), -cos (2*θ))
+
 rot90 :: Num a => Isometry a
 rot90 = MatrixIso (0, -1, 1, 0)
+
+reflX :: Num a => Isometry a
+reflX = MatrixIso (1,0,0,-1)
 
 applyIso :: Num a => Isometry a -> (a,a) -> (a,a)
 applyIso (MatrixIso (a,b,c,d)) (x,y) = (a*x+b*y, c*x+d*y)
