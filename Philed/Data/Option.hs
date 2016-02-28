2406ab6c3f8e62d7dc8bd2e55cc7462954933d90
Phil Scott <me@philscotted.com>
2016-02-28 12:17:05 +0000

    Initial commit.

---
+++ Philed/Data/Option.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+-- | Wrapper of Maybe to be an instance of MonadError.
+
+module Philed.Data.Option where
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.Except
+import Control.Monad.Fix
+import Data.Traversable
+
+newtype Option a = Option { runOption :: Maybe a }
+                 deriving (Eq, Monad, Functor, Ord, Read, Show, MonadFix
+                          ,Alternative, Applicative, Foldable, Traversable
+                          ,Monoid, MonadPlus)
+
+instance MonadError () Option where
+  throwError () = Option Nothing
+  catchError (Option Nothing) f = f ()
+  catchError x _                = x
