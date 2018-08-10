1c036ce09559e42c02c2c87f11e9cb57651aea79
Phil Scott <phil.scott@symphonicsoft.com>
2018-08-10 18:49:44 +0100

    Don't depend on being able to build.

--- default.nix
+++ default.nix
@@ -5,9 +5,10 @@ let
    myemacs =
      with pkgs.emacsPackages; with pkgs.emacsPackagesNg; pkgs.emacsWithPackages
       [ haskell-mode helm-projectile magit paredit ];
-   myhaskell =
-     pkgs.haskellPackages.ghcWithPackages (p: with p; [
-     cabal-install hlint (pkgs.haskellPackages.callPackage philedcommon {})
+   myhaskell = pkgs.haskellPackages.ghcWithPackages (p: with p; [
+     cabal-install hlint
+     array base binary comonad containers free mtl parsec QuickCheck
+     semigroupoids semigroups semiring-simple transformers
     ]);
 in with pkgs; stdenv.mkDerivation {
   name = "Philed";
