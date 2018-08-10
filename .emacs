9c58de655e345264ba1164627bd9e76d7cca5c66
Phil Scott <phil.scott@symphonicsoft.com>
2018-08-10 18:48:17 +0100

    Drop ghc-mod (not working on nix atm).

--- .emacs
+++ .emacs
@@ -5,7 +5,5 @@
 (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 (require 'magit)
-(require 'ghc)
 (require 'helm-config)
 (require 'helm-projectile)
-
--- default.nix
+++ default.nix
@@ -4,10 +4,10 @@ let
    philedcommon = import ./PhiledCommon.nix;
    myemacs =
      with pkgs.emacsPackages; with pkgs.emacsPackagesNg; pkgs.emacsWithPackages
-      [ ghc-mod haskell-mode helm-projectile magit paredit ];
+      [ haskell-mode helm-projectile magit paredit ];
    myhaskell =
      pkgs.haskellPackages.ghcWithPackages (p: with p; [
-      ghc-mod cabal-install hlint (pkgs.haskellPackages.callPackage philedcommon {})
+     cabal-install hlint (pkgs.haskellPackages.callPackage philedcommon {})
     ]);
 in with pkgs; stdenv.mkDerivation {
   name = "Philed";
